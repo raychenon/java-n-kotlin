f70ace01a93cb9db3b4a386b9c0d5e8ea4cc4fe4
raychenon <raymond.chenon@gmail.com>
2025-03-23 23:59:52 +0100

    add extension txt

---
+++ src/main/java/com/raychenon/javankotlin/pojo/PersonJ.java
@@ -0,0 +1,11 @@
+package com.raychenon.javankotlin.pojo;
+
+import java.util.Optional;
+
+public class PersonJ {
+
+    private String name;
+    private Optional<PersonJ> father;
+    private Optional<PersonJ> mother;
+    
+}
