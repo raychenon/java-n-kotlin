6dc7b63281c74fa681265dc6f23d34d145d208dd
raychenon <raymond.chenon@gmail.com>
2025-03-23 18:16:13 +0100

    add maven . Remove Lombok in UserJ

--- src/main/java/com/raychenon/javankotlin/pojo/UserJ.java
+++ src/main/java/com/raychenon/javankotlin/pojo/UserJ.java
@@ -1,15 +1,15 @@
 package com.raychenon.javankotlin.pojo;
 
-import lombok.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
-@EqualsAndHashCode
-@ToString
-@Getter
 public class UserJ {
+
     private String firstName;
     private String lastName;
     private String id;
-    @Setter
+
     private boolean isActive;
 
     public UserJ(String firstName, String lastName, String id, boolean isActive) {
@@ -17,6 +17,37 @@ public class UserJ {
         this.lastName = lastName;
         this.id = id;
         this.isActive = isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public boolean equals(Object object) {
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        UserJ userJ = (UserJ) object;
+        return isActive == userJ.isActive && java.util.Objects.equals(firstName, userJ.firstName) && java.util.Objects.equals(lastName, userJ.lastName) && java.util.Objects.equals(id, userJ.id);
+    }
+
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), firstName, lastName, id, isActive);
     }
 
 }
